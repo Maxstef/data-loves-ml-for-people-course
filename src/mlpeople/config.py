88a8b69a449e6c6d92014737877ae67480d93bd5
Maksym Stefanko <casstefaks@gmail.com>
2026-03-31 17:30:45 +0300

    start practice llm langchain basic

--- src/mlpeople/config.py
+++ src/mlpeople/config.py
@@ -4,3 +4,5 @@ GOOGLE_SERVICE_ACCOUNT_EMAIL = os.getenv("GOOGLE_SERVICE_ACCOUNT_EMAIL")
 GOOGLE_PRIVATE_KEY = os.getenv("GOOGLE_PRIVATE_KEY")
 GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
 GOOGLE_SCOPE = "https://www.googleapis.com/auth/drive.readonly"
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
